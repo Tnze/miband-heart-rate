63188c61499ea21bb4cb909a1d37711a786fb66d
Tnze <cjd001113@outlook.com>
2023-10-16 22:49:51 +0800

    submit everything

---
+++ README.md
@@ -0,0 +1,13 @@
+# MiBand Heart Rate Demo
+
+A Demo of reading "Activity heart rate sharing" of Xiaomi Smart Band 6. Enable the option in official App is required.
+
+接收小米手环6 “运动心率广播” Demo，测试前需要在小米运动健康App中开启手环的该选项。
+
+MiBand 4 and 5 would also work.
+
+小米手环4和5应该也可以，但我没有测试
+
+## screenshot
+
+![Alt text](doc/screenshot.png)
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+
+use bluest::{Adapter, AdvertisingDevice};
+use futures_lite::stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let adapter = Adapter::default()
+        .await
+        .ok_or("Bluetooth adapter not found")?;
+    adapter.wait_available().await?;
+
+    println!("starting scan");
+    let mut scan = adapter.scan(&[]).await?;
+    println!("scan started");
+    while let Some(discovered_device) = scan.next().await {
+        handle_device(discovered_device)
+    }
+    Ok(())
+}
+
+fn handle_device(discovered_device: AdvertisingDevice) {
+    if let Some(manufacturer_data) = discovered_device.adv_data.manufacturer_data {
+        if manufacturer_data.company_id != 0x0157 {
+            return;
+        }
+        let name = discovered_device
+            .device
+            .name()
+            .unwrap_or(String::from("(unknown)"));
+        let rssi = discovered_device.rssi.unwrap_or_default();
+
+        println!(
+            "{name} ({rssi}dBm) Heart Rate: {:?}",
+            manufacturer_data.data[3]
+        );
+    }
+}
