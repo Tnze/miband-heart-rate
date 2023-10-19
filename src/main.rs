25e55d25bf8191282eedbba904435cda94025018
Tnze <cjd001113@outlook.com>
2023-10-19 19:25:52 +0800

    optimize web code

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,36 @@
-use std::{net::SocketAddr, sync::Arc};
+use std::net::SocketAddr;
 
 use bluest::{Adapter, AdvertisingDevice};
 use futures_lite::stream::StreamExt;
-use tokio::sync::{
-    watch::{self, Sender},
-    Mutex,
-};
+use tokio::sync::watch::{self, Receiver, Sender};
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     let (tx, rx) = watch::channel(0);
-    let rx = Arc::new(Mutex::new(rx));
-
-    tokio::spawn(scan_bluetooth(tx));
-
-    let get_heart_rate = move || {
-        let rx = rx.clone();
-        async move {
-            let mut rx = rx.lock().await;
-            rx.changed().await.unwrap();
-            let heart_rate = *rx.borrow();
-            heart_rate.to_string()
-        }
-    };
-
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let root = warp::path::end().map(|| warp::reply::html(include_str!("../web/index.html")));
-    let heartrate = warp::path!("heartrate").then(get_heart_rate);
-
-    let socket_addr: SocketAddr = ([127, 0, 0, 1], 3030).into();
-    println!("Start listening at: http://{socket_addr:?}");
-    warp::serve(warp::get().and(root.or(heartrate)))
-        .run(socket_addr)
-        .await;
+    tokio::join!(ble_scanner(tx), web_server(rx));
 }
 
-async fn scan_bluetooth(tx: Sender<i32>) {
+async fn web_server(rx: Receiver<i32>) {
+    let root = warp::path::end().map(|| warp::reply::html(include_str!("../web/index.html")));
+    let heartrate = warp::path!("heartrate").then(move || {
+        let mut rx = rx.clone();
+        async move {
+            drop(rx.borrow_and_update());
+            rx.changed().await.unwrap();
+            warp::reply::json(&*rx.borrow())
+        }
+    });
+
+    let socket_addr: SocketAddr = ([127, 0, 0, 1], 3030).into();
+    println!("Start listening at http://{socket_addr:?}");
+
+    warp::serve(warp::get().and(root).or(heartrate))
+        .run(socket_addr)
+        .await
+}
+
+async fn ble_scanner(tx: Sender<i32>) {
     let adapter = Adapter::default()
         .await
         .ok_or("Bluetooth adapter not found")
@@ -64,6 +58,10 @@ fn handle_device(discovered_device: AdvertisingDevice) -> Option<i32> {
         .unwrap_or(String::from("(unknown)"));
     let rssi = discovered_device.rssi.unwrap_or_default();
     let heart_rate = manufacturer_data.data[3];
+    let heart_rate = match heart_rate {
+        0xFF => None,
+        x => Some(x.into()),
+    };
     println!("{name} ({rssi}dBm) Heart Rate: {heart_rate:?}",);
-    Some(heart_rate.into())
+    heart_rate
 }
